e39a16e2c15f2187e9e9c4b1e252579e59a8685a
zabi <husseinzabi389@gmail.com>
2022-07-20 11:20:38 +0300

    basic design home page

--- src/component/RandomeMovie.jsx
+++ src/component/RandomeMovie.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import useFetch from "../useFetch";
 import { Link } from "react-router-dom";
+import { BsFillPlayCircleFill, BsArrowDownCircleFill } from "react-icons/bs";
+import { GrAdd, GrAddCircle } from "react-icons/gr";
 function RandomeMovie() {
   const [randomMovie, setRandomMovie] = useState(null);
   const { data, loading, err } = useFetch("popular", "1");
@@ -13,7 +15,21 @@ function RandomeMovie() {
           backgroundImage: `url("https://image.tmdb.org/t/p/w500${data[randomNumber].poster_path}")`,
           backgroundSize: "cover  ",
         }}>
-        <h2 className="main_welcome_masseage">welcome to the movie project</h2>
+        <div className="main_random_movie_controler">
+          <h2 className="random_movie_title">
+            {data[randomNumber].original_title}
+          </h2>
+          <div className="main_controler_play_and_add_to_list">
+            <Link className="play_link" to="/home">
+              <BsFillPlayCircleFill />
+              <h4 className="random_movie_play_text">Play</h4>
+            </Link>
+            <div className="mian_add_to_list_handler">
+              <BsArrowDownCircleFill style={{ color: "#fff" }} />
+              <h4 className="add_to_list_test">My List</h4>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/navigationSystem/NavigationSystem.jsx
+++ src/navigationSystem/NavigationSystem.jsx
@@ -1,47 +1,50 @@
-import { Link, useParams } from "react-router-dom"
-import { useState } from "react"
-import { BiSearchAlt} from "react-icons/bi"
-import {MdHomeFilled} from "react-icons/md"
-import {FaWpexplorer} from "react-icons/fa"
-import {HiClipboardList} from "react-icons/hi"
-import {CgProfile} from "react-icons/cg"
-import '../style/style.css'
-export default function NavigationSystem (){
-    let {id} = useParams();
-    return (
-            <ul className="ulbar">
-                <li className="ulbar_links">
-                    <Link className="navigation_links home_link" to="/home">
-                        <MdHomeFilled />
-                        <h3 className="mian_home_link">
-                            Home
-                        </h3>
-                    </Link>
-                </li>
-                <li className="ulbar_links">
-                    <Link className="navigation_links explor_link" to="/home/explor">
-                        <FaWpexplorer />
-                        <h3>
-                        Explor
-                        </h3>
-                    </Link>
-                </li>
-                <li className="ulbar_links">
-                    <Link className="navigation_links mylist_link" to="/home/mylist">
-                        <HiClipboardList />
-                        <h3>
-                            My List
-                        </h3>
-                    </Link>
-                </li>
-                <li className="ulbar_links">
-                    <Link className="navigation_links mylist_link" to="/home/profile">
-                        <CgProfile />
-                        <h3>
-                        profile
-                        </h3>
-                    </Link>
-                </li>
-            </ul>
-    )
+import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import { MdHomeFilled } from "react-icons/md";
+import { FaWpexplorer } from "react-icons/fa";
+import { HiClipboardList } from "react-icons/hi";
+import { CgProfile } from "react-icons/cg";
+import { BiCameraMovie } from "react-icons/bi";
+import "../style/style.css";
+export default function NavigationSystem() {
+  let { id } = useParams();
+  return (
+    <>
+      <nav className="topnavbar">
+        <Link className="nav_home_title_link" to="/home">
+          <BiCameraMovie />
+          <div className="main_title">
+            Mo<span>vie</span>
+          </div>
+        </Link>
+      </nav>
+      <ul className="ulbar">
+        <li className="ulbar_links">
+          <Link className="navigation_links home_link" to="/home">
+            <MdHomeFilled />
+            <h3 className="mian_home_link">Home</h3>
+          </Link>
+        </li>
+        <li className="ulbar_links">
+          <Link className="navigation_links explor_link" to="/home/explor">
+            <FaWpexplorer />
+            <h3>Explor</h3>
+          </Link>
+        </li>
+        <li className="ulbar_links">
+          <Link className="navigation_links mylist_link" to="/home/mylist">
+            <HiClipboardList />
+            <h3>My List</h3>
+          </Link>
+        </li>
+        <li className="ulbar_links">
+          <Link className="navigation_links mylist_link" to="/home/profile">
+            <CgProfile />
+            <h3>profile</h3>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 }
